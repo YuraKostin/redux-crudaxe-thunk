820bb2fda0904b38ee93e407bf19bca08d62f190
Yura Kostin <yurakostinm@yandex.ru>
2020-03-28 00:09:38 +0300

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,18 @@ module.exports = {
         library: 'Crudaxe',
         libraryTarget: 'umd',
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, //Regular expression
+                exclude: /(node_modules|bower_components)/,//excluded node_modules
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']  //Preset used for env setup
+                    }
+                }
+            }
+        ],
+    },
 };
